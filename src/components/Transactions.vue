4ff7876a873e1cb3b5e287167bf0c9e30dc5dd31
Brayan Garcia <brayan.garcia@puntopay.co>
2021-12-02 20:55:49 -0500

    fininigh transafactions

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -1,14 +1,45 @@
 <template>
     <div class="transactionContainer">
+        <!-- Account -->
         <div class="accountInfo shadow">
             <div class="titleContainer">
                 <img src="../assets/bank.png">
                 <h3>Mi cuenta bancaria</h3>
             </div>
-            <span class="money">{{accountByUsername.balance}}</span>
+            <span class="money">$ {{formatPrice(accountByUsername.balance)}}</span>
             <span><b>Usuario: </b> {{username}}</span>
-            <span><b>Fecha de creación:</b> {{accountByUsername.lastChange}}</span>
+            <span><b>Fecha de creación:</b> 
+                <span v-if="accountByUsername.lastChange">{{accountByUsername.lastChange}}</span>
+                <span v-if="!accountByUsername.lastChange"> No registra</span>
+            </span>
         </div>
+        <!-- Creating Transaction -->
+        <div class="transactionsSection">
+            <h4>Transacciones</h4>
+            <input v-model="transaction.usernameOrigin" class="form-control"  placeholder="Usuario Origen">
+            <input v-model="transaction.usernameDestiny" class="form-control"  placeholder="Usuario Destino">
+            <input v-model="transaction.value" class="form-control"  placeholder="Valor">
+            <button v-on:click="createTransaction" class="btn btn-primary">Envíar</button>
+        </div>
+        <!-- Transactions -->
+        <table class="table table-striped transactionsTable">
+            <thead>
+                <tr>
+                    <th>Usuario origen</th>
+                    <th>Usuario destino</th>
+                    <th>Valor</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="transaction in transactionByUsername" :key="transaction.id">
+                    <td>{{transaction.usernameOrigin}}</td>
+                    <td>{{transaction.usernameDestiny}}</td>
+                    <td >
+                        <span  v-bind:class="{'negativeValue': transaction.usernameOrigin == username}" class="transactionValue" v-if="transaction.value">$ {{formatPrice(transaction.value)}}</span>
+                        <span v-if="!transaction.value">0</span></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -23,7 +54,13 @@ export default {
             username: localStorage.getItem("username") || "none",
             accountByUsername: {
                 balance: "",
-                lastChange: ""
+                lastChange: "",
+            },
+            transactionByUsername: [],
+            transaction: {
+                usernameOrigin: "",
+                usernameDestiny: "",
+                value: 0
             }
         }
     }, 
@@ -37,17 +74,69 @@ export default {
                     }
                 }
             `,
-            variables(){
+            variables() {
                 return {
-                    username: this.username
-                }
-            }
+                    username: this.username,
+                };
+            },
+        },
+        transactionByUsername: {
+            query: gql`
+                query TransactionByUsername($username: String!) {
+                transactionByUsername(username: $username) {
+                            id
+                            date
+                            usernameDestiny
+                            usernameOrigin
+                            value
+                        }
+                        }
+                    `,
+                    variables(){
+                        return {
+                            username: this.username
+                        }
+                    }
         }
     },
     methods: {
+        formatPrice(value) {
+            let val = (value / 1).toFixed(2).replace(".", ",");
+            return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+        },
+        createTransaction: async function(){
+            this.transaction.value = +this.transaction.value; // Convierte a entero porque el apigateway lo espera asi
+            await this.$apollo.mutate({
+                mutation: gql`
+                 mutation CreateTransaction($transaction: TransactionInput!) {
+                    createTransaction(transaction: $transaction) {
+                        id
+                        usernameDestiny
+                        usernameOrigin
+                        value
+                    }
+                }`,
+                variables: {
+                    transaction: this.transaction
+                }
+            })
+            .then((result)=>{
+                this.$apollo.queries.accountByUsername.refetch();
+                 this.$apollo.queries.transactionByUsername.refetch();
+                 this.transaction.usernameDestiny = ""
+                 this.transaction.usernameOrigin = ""
+                 this.transaction.value = 0
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
         
+        }
     }, 
-    created: function () {}
+    created: function () {
+        this.$apollo.queries.accountByUsername.refetch();
+        this.$apollo.queries.transactionByUsername.refetch();
+    }
 };
 </script>
 
@@ -76,5 +165,27 @@ export default {
 
     .money{
             font-size: 35px;
+    }
+
+    .transactionsTable{
+        margin-top: 25px;
+    }
+
+    .transactionValue{
+        color: green;
+    }
+
+    .negativeValue{
+        color: red;
+    }
+
+    .transactionsSection{
+        margin-top: 50px;
+    }
+
+    .transactionsSection input{
+        margin-right: 15px;
+        width: 25%;
+        display: inline;
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ const authLink = setContext((_, { headers }) => {
     return {
         headers: {
             ...headers,
-            "Authorization": localStorage.getItem("token_access") || ""
+            "Authorization": localStorage.getItem("token") || ""
         }
     }
 })
@@ -28,3 +28,4 @@ const apolloProvider = new createApolloProvider({
 })
 
 createApp(App).use(router).use(apolloProvider).mount('#app')
+
